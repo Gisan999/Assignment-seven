6589e4e2c55e63d461cf3c7ef41850c0d9f27fcf
Gisan999 <ruheatheking3@gmail.com>
2023-09-15 04:20:02 +0600

    added the fake data from chat gpt

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Home from './Components/Home/Home'
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1 className='text-5xl'>hello girls</h1>
+   <Home></Home>
+    </>
+  )
+}
+
+export default App
