1733036fff0c780077fe0496128902f87c603295
Gisan999 <ruheatheking3@gmail.com>
2023-09-15 18:28:01 +0600

    credit hour remaining and total credit hour end of functionality

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -2,30 +2,29 @@
 
 
 // eslint-disable-next-line react/prop-types
-const Cart = ({ selectCourse }) => {
-
-    // console.log(selectCourse)
+const Cart = ({ selectCourse, totalCredit, remaining, totalCost }) => {
     return (
         <div className="bg-sky-50 p-5 rounded-lg">
             <div>
-                <h1 className="font-bold text-xl text-[#2F80ED] border-b-2 border-gray-300 pt-3 pb-3">Credit Hour Remaining 7 hr</h1>
-
+                <h1 className="font-bold text-xl text-[#2F80ED] border-b-2 border-gray-300 pt-3 pb-3">Credit Hour Remaining {remaining} hr</h1>
             </div>
             <div>
                 <h1 className="text-xl font-bold py-3">Course Name</h1>
                 <div className="pb-3 space-y-1 font-medium italic text-gray-500">
                     {
                         selectCourse.map((courseName, index) => (
-                            <p key={courseName['ID']}>{index+1}   {courseName['Course_Name']}</p>
+                            <p key={courseName['ID']}>{index + 1}   {courseName['Course_Name']}</p>
                         ))
                     }
                 </div>
-
             </div>
             <div>
-                <h1 className="font-medium border-t-2 border-gray-300 text-lg py-2 border-b-2">Total Credit Hour : 13</h1>
+                <h1 className="font-medium border-t-2 border-gray-300 text-lg py-2 border-b-2">Total Credit Hour : {totalCredit}</h1>
             </div>
-         
+
+            <div className="pt-3">
+                <h1 className="font-semibold text-xl">Total Price: {totalCost} USD</h1>
+            </div>
         </div>
     );
 };
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,71 +3,86 @@ import { useState } from "react";
 import Cart from "../Cart/Cart";
 import { useEffect } from "react";
 import Swal from 'sweetalert2'
-
-
 const Home = () => {
-
     const [jsonData, setJsonData] = useState([]);
     const [selectCourse, setSelectCourse] = useState([]);
-    
-
+    const [totalCredit, setTotalCreddit] = useState(0);
+    const [remaining, setRemaining] = useState(20);
+    const [totalCost, setTotalCost] = useState(0)
     useEffect(() => {
         fetch('fakeData.json')
             .then(res => res.json())
             .then(data => setJsonData(data))
     }, [])
-
     const handleSelectCourse = course => {
-        const isExist = selectCourse.find((item) => item['ID']=== course['ID']);
-        // console.log(isExist)
-        if(isExist){
-             // eslint-disable-next-line no-unreachable
-             let timerInterval
+        const isExist = selectCourse.find((item) => item['ID'] === course['ID']);
+        let sum = course['Credit'];
+        let total = course['Price'];
+        if (isExist) {
+            let timerInterval
             Swal.fire({
-              title: 'You already have this course',
-              html: 'I will close in <b></b> milliseconds.',
-              timer: 2000,
-              timerProgressBar: true,
-              didOpen: () => {
-                Swal.showLoading()
-                const b = Swal.getHtmlContainer().querySelector('b')
-                timerInterval = setInterval(() => {
-                  b.textContent = Swal.getTimerLeft()
-                }, 100)
-              },
-              willClose: () => {
-                clearInterval(timerInterval)
-              }
-            }).then((result) => {
-              /* Read more about handling dismissals below */
-              if (result.dismiss === Swal.DismissReason.timer) {
-                console.log('I was closed by the timer')
-              }
+                title: 'You already have this course',
+                html: 'I will close in <b></b> milliseconds.',
+                timer: 2000,
+                timerProgressBar: true,
+                didOpen: () => {
+                    Swal.showLoading()
+                    const b = Swal.getHtmlContainer().querySelector('b')
+                    timerInterval = setInterval(() => {
+                        b.textContent = Swal.getTimerLeft()
+                    }, 100)
+                },
+                willClose: () => {
+                    clearInterval(timerInterval)
+                }
             })
+            
+            // .then((result) => {
+            //     /* Read more about handling dismissals below */
+            //     // if (result.dismiss === Swal.DismissReason.timer) {
+            //     //     console.log('I was closed by the timer')
+            //     // }
+            // })
 
-        }else{
-
+        } else {
+            selectCourse.forEach((count) => {
+                total = total + count['Price']
+            })
+            selectCourse.forEach((item) => {
+                sum = sum + item['Credit']
+            })
+            const TotalRemaining = 20 - sum;
+            if (TotalRemaining < 0) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Your credit is over!',
+                })
+                return;
+            }
+            if (sum > 20) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'You cannot go above 20 hours!',
+                })
+            }
+            setTotalCost(total)
+            setRemaining(TotalRemaining)
+            setTotalCreddit(sum)
             setSelectCourse([...selectCourse, course]);
         }
 
     }
-// console.log(selectCourse)
-    // console.log(jsonData)
-
     return (
         <div className="container mx-auto my-8">
-
             <div className="flex flex-col lg:flex-row justify-between gap-12">
-
-
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 lg:w-3/4 px-8 lg:px-0">
-
                     {
-                        // eslint-disable-next-line no-unused-vars
                         jsonData.map(card => (
                             <div key={card['id']} className="bg-gray-100  p-4 space-y-4 rounded-lg" >
                                 <div className="flex justify-center">
-                                    <img src={card['Cover_Image']}alt="" />
+                                    <img src={card['Cover_Image']} alt="" />
                                 </div>
                                 <h3 className="text-center font-semibold">{card['Course_Name']}</h3>
                                 <p className="text-center"><small>{card['Details']}</small></p>
@@ -76,23 +91,19 @@ const Home = () => {
                                     <p>Credit: {card['Credit']}hr</p>
                                 </div>
                                 <div className="flex justify-center">
-                                    <button 
-                                    onClick={()=>handleSelectCourse(card)}
-                                     className="bg-[#2F80ED] text-white font-medium text-lg py-2 px-32 rounded-xl">Select</button>
+                                    <button
+                                        onClick={() => handleSelectCourse(card)}
+                                        className="bg-[#2F80ED] text-white font-medium text-lg py-2 px-32 rounded-xl">Select</button>
                                 </div>
                             </div>
                         ))
                     }
-
                 </div>
-
                 <div className=" md:w-2/4 lg:w-1/4 px-8 lg:px-0">
-                    <Cart selectCourse={selectCourse}></Cart>
+                    <Cart totalCost={totalCost} remaining={remaining} selectCourse={selectCourse} totalCredit={totalCredit
+                    }></Cart>
                 </div>
-
             </div>
-
-
         </div >
     );
 };
